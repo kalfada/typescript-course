60c34c024f4783165a5bdff03c685cd8260340f0
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-04-26 09:18:23 +0300

    update

--- ex20-typescript-on-the-web/src/ts/index.ts
+++ ex20-typescript-on-the-web/src/ts/index.ts
@@ -1,1 +1,32 @@
+import { Pizza, PizzaProps } from "./models/Pizza";
 
+const rootElement = document.querySelector('.root')!
+
+const createPizzaTemplate = (pizza: PizzaProps): string => {
+    return `
+    <div class="pizza">
+        <h2>${pizza.title}</h2>
+        <p class="toppings">${pizza.toppings.join(', ')}</p>
+        <p>${pizza.description}</p>
+        <span>${pizza.price}$</span>
+    </div>
+    `
+}
+
+const render = (templates: string[], parent: Element): void => {
+    const templateElement = document.createElement('template')
+
+    for (const t of templates) {
+        templateElement.innerHTML += t
+    }
+
+    parent.append(templateElement.content)
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const pizzas = await Pizza.loadAll()
+
+    const pizzaTemplates = pizzas.map(createPizzaTemplate)
+
+    render(pizzaTemplates, rootElement)
+})
--- ex20-typescript-on-the-web/src/ts/services/DataResource.ts
+++ ex20-typescript-on-the-web/src/ts/services/DataResource.ts
@@ -1,8 +1,22 @@
+import axios from 'axios'
+
 export class DataResource<T> {
     constructor(private endpoint: string) { }
 
-    async loadAll() { }
-    async loadOne(id: number) { }
-    async delete(id: number) { }
-    async save(data: T) { }
+    async loadAll(): Promise<T[]> {
+        const { data } = await axios.get(this.endpoint)
+        return data
+    }
+    async loadOne(id: number): Promise<T> {
+        const { data } = await axios.get(`${this.endpoint}/${id}`)
+        return data
+    }
+    async delete(id: number): Promise<Response> {
+        const { data } = await axios.delete(`${this.endpoint}/${id}`)
+        return data
+    }
+    async save(data: T) {
+        const res = await axios.post(this.endpoint, data)
+        return res.data
+    }
 }
